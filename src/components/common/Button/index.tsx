ce2e0b73e6ffafc927af8294c73e9d2a9b8f22af
useonglee <useong0830@naver.com>
2022-04-17 23:37:11 +0900

    feat: app init setup

---
+++ src/components/common/Button/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Button() {
+  return <button>Button</button>;
+}
+
+export default Button;
---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import Button from '@components/common/Button';
+
+const Home: NextPage = () => {
+  return <Button />;
+};
+
+export default Home;
