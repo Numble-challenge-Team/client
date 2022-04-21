742f664164718e56d6d0b21933c70f39677e0d34
useonglee <useong0830@naver.com>
2022-04-21 22:56:43 +0900

    #1 feat: 코드 컨벤션 정의

---
+++ src/components/common/Button/Button.tsx
@@ -0,0 +1,7 @@
+import * as Styled from './ButtonStyle';
+
+function Button() {
+  return <Styled.ButtonStyle>button</Styled.ButtonStyle>;
+}
+
+export default Button;
---
+++ src/components/common/Button/ButtonStyle.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const ButtonStyle = styled.button`
+  background-color: ${({ theme }) => theme.color.white};
+`;
