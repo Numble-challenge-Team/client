966e925f6edd51e29bdfc2e7fac6091b6e185492
useonglee <useong0830@naver.com>
2022-04-24 01:28:00 +0900

    #1 feat: styled-components SSR 적용을 위한 document 파일 커스터마이징

--- src/_document.tsx
+++ src/_document.tsx
@@ -1,18 +1,29 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx: any) {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+        });
 
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: (App: any) => App,
-        enhanceComponent: (Component: any) => Component,
-      });
-
-    const initialProps = await Document.getInitialProps(ctx);
-
-    return initialProps;
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>,
+        ],
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 
   render() {
