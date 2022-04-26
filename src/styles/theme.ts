7abd9fa3ca55ef3fc3184f84aa7d0cb7291fbd99
sonwonjae <sonwj091552@gmail.com>
2022-04-26 22:04:33 +0900

    #9 style: 컬러시스템 구축

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,25 @@
 const color = {
+  primary: {
+    100: '#CCF5FA',
+    200: '#99EEF5',
+    300: '#66E6EF',
+    400: '#33DFEA',
+    500: '#00D8E5',
+  },
+  primary_shadow: {
+    light: '#00D8E530',
+    dark: '#00D8E560',
+  },
   white: '#ffffff',
+  gray: {
+    100: '#F0F0F0',
+    200: '#C4C4C4',
+    300: '#808080',
+    400: '#4A4A4A',
+    500: '#47484A',
+  },
   black: '#000000',
+  dim: '#00000040',
 };
 
 const Theme = {
