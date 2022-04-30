d6e990ecf1bda2acdccab9f6899992cf1d3f179b
useonglee <useong0830@naver.com>
2022-05-01 02:10:24 +0900

    #11 feat: 버튼 공통 컴포넌트 추가

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,7 +1,30 @@
+import { PropsWithChildren } from 'react';
 import * as Styled from './ButtonStyle';
 
-function Button() {
-  return <Styled.ButtonStyle>button</Styled.ButtonStyle>;
+export interface ButtonPropsType {
+  type: 'button' | 'submit' | 'reset';
+  size: string;
+  margin?: string;
+  backColor: string;
+  radius: string;
+  clickEvent?: () => void;
 }
+
+function Button(props: PropsWithChildren<ButtonPropsType>) {
+  const { children, clickEvent, ...rest } = props;
+
+  return (
+    <Styled.ButtonStyle {...rest} onChange={clickEvent}>
+      {children}
+    </Styled.ButtonStyle>
+  );
+}
+
+Button.defaultProps = {
+  type: 'button',
+  size: 'L',
+  backColor: 'primary',
+  radius: '10',
+};
 
 export default Button;
--- src/components/common/Button/ButtonStyle.ts
+++ src/components/common/Button/ButtonStyle.ts
@@ -1,6 +1,52 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const ButtonStyle = styled.button`
-  font-family: 'NanumSquareB';
-  background-color: ${({ theme }) => theme.color.white};
+export const ButtonStyle = styled.button<{ size: string; margin?: string; backColor: string; radius: string }>`
+  ${({ theme, size, margin, backColor, radius }) => {
+    const { color } = theme;
+
+    return css`
+      ${handleSize(size)}
+      ${handleColor(color, backColor)}
+      margin: ${margin};
+      color: ${color.gray['700']};
+      border-radius: ${radius}px;
+    `;
+  }}
 `;
+
+const handleSize = (size: string) => {
+  switch (size) {
+    case 'S':
+      return css`
+        width: 12rem;
+        height: 3.2rem;
+      `;
+    case 'M':
+      return css`
+        width: 25rem;
+        height: 4rem;
+      `;
+    case 'L':
+      return css`
+        width: 33.5rem;
+        height: 4.8rem;
+      `;
+    default:
+      return css``;
+  }
+};
+
+const handleColor = (color: any, backColor: any) => {
+  const { primary, gray } = color;
+
+  switch (backColor) {
+    case 'primary':
+      return css`
+        background-color: ${primary['500']};
+      `;
+    default:
+      return css`
+        background-color: ${gray['200']};
+      `;
+  }
+};
