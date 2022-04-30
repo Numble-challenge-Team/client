151899f2e40f0bd76e90d3a4c2d320a41bacefa8
useonglee <useong0830@naver.com>
2022-05-01 02:08:48 +0900

    #5 feat: 회원가입 시 닉네임 중복, 정규식, 데이터 오류 예외 처리

--- src/pages/[signup]/nickname.tsx
+++ src/pages/[signup]/nickname.tsx
@@ -1,15 +1,18 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useRecoilState } from 'recoil';
 import { useRouter } from 'next/router';
 
-import { Input } from '@components/Common';
+import { Button, Input } from '@components/Common';
+import Layout from '@components/Layout/Layout';
 
 import { FormRegisterType, SignupInfoType } from '@/types/signup';
 
 import { userSingupState } from '@store/signup';
 import { NICKNAME_VALIDATION } from '@constants/validation';
 import { useSignupQuery, useValidationSignupQuery } from '@api/queries/signup';
+
+import * as Styled from '@components/Signup/SignupPageStyle';
 
 function SignupNicknamePage() {
   const router = useRouter();
@@ -19,35 +22,63 @@ function SignupNicknamePage() {
     formState: { errors },
     handleSubmit,
   } = useForm<FormRegisterType>();
+  const [formErrorState, setFormErrorState] = useState<boolean>(false);
+  const [NincknameErrorMessage, setNincknameErrorMessage] = useState<string>('');
   const [userNicknameData, setUserNicknameData] = useState<Pick<SignupInfoType, 'nickname'>>({ nickname: '' });
   const [signupInfo, setSignupInfo] = useRecoilState<SignupInfoType>(userSingupState);
 
-  const { data } = useValidationSignupQuery('nickname', userNicknameData, {
+  const { error } = useValidationSignupQuery('nickname', userNicknameData, {
+    enabled: !!userNicknameData.nickname,
+    retry: 0,
     onSuccess: () => {
       setSignupInfo({ ...signupInfo, nickname: userNicknameData.nickname });
     },
-    onError: (error: any) => {
-      // 닉네임 중복처리 에러 로직
-    },
   });
 
-  const signupData = useSignupQuery(signupInfo, {
+  const fetchSignup = useSignupQuery(signupInfo, {
     enabled: signupInfo.nickname !== '',
+    retry: 0,
     onSuccess: () => {
       // 회원가입 성공 시
       router.push('/');
     },
-    onError: (error: any) => {
-      // 회원가입 실패
-    },
   });
 
-  const handleNicknameSubmit = (userNickname: FormRegisterType) => {
+  const handleNicknameSubmit = useCallback((userNickname: FormRegisterType) => {
     setUserNicknameData({ nickname: userNickname.nickname });
-  };
+  }, []);
+
+  useEffect(() => {
+    // 회원가입 하는 도중에 새로고침할 경우
+    if (fetchSignup.error && (signupInfo.email === '' || signupInfo.password === '')) {
+      setSignupInfo({
+        email: '',
+        password: '',
+        nickname: '',
+      });
+
+      alert('죄송합니다. 처음부터 다시 진행부탁드립니다.');
+      router.push('/signup/email');
+    }
+  }, [fetchSignup.error]);
+
+  useEffect(() => {
+    // 중복처리
+    if (error && error.response !== undefined) {
+      setFormErrorState(true);
+      setNincknameErrorMessage(error.response.data.message);
+    }
+  }, [userNicknameData, error]);
+
+  useEffect(() => {
+    // 닉네임 규칙 예외처리
+    if (errors.nickname?.message) {
+      setFormErrorState(false);
+    }
+  }, [errors.nickname?.message]);
 
   return (
-    <>
+    <Layout hasHeader={false}>
       <p>거의 다 왔어요!</p>
       <p>나만의 닉네임으로 시작해요.</p>
       <form onSubmit={handleSubmit(handleNicknameSubmit)}>
@@ -58,13 +89,17 @@ function SignupNicknamePage() {
           register={register}
           pattern={NICKNAME_VALIDATION}
           placeholderText="별명"
+          hasErrorDisplay={formErrorState || !!errors.nickname?.message}
           required
         />
-        {errors && errors.nickname?.message}
+        {errors && <Styled.ErrorMessage>{errors.nickname?.message}</Styled.ErrorMessage>}
+        {formErrorState && <Styled.ErrorMessage>{NincknameErrorMessage}</Styled.ErrorMessage>}
 
-        <button type="submit">완료</button>
+        <Button type="submit" margin="3.6rem 0 0 0">
+          완료
+        </Button>
       </form>
-    </>
+    </Layout>
   );
 }
 
