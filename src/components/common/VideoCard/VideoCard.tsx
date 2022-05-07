a2c362c432229c3046ffc68f36dd22d86a2792b4
sonwonjae <sonwj091552@gmail.com>
2022-05-07 19:36:31 +0900

    #26 style: MockDB기반으로 스타일링 우선 구현

---
+++ src/components/common/VideoCard/VideoCard.tsx
@@ -0,0 +1,66 @@
+import Link from 'next/link';
+
+import { PropsWithChildren } from 'react';
+
+import * as VideoCardStyle from './VideoCardStyle';
+import { Icon } from '@components/Common';
+
+interface VideoCardProps {
+  cardInfo: {
+    videoId: string;
+    uploadThumbNail: {
+      storeThumbName: string;
+      uploadThumbUrl: string;
+    };
+    title: string;
+    duration: number;
+    nickname: string;
+    view: number;
+    likes: number;
+    created_at: string;
+  };
+}
+
+function VideoCard({
+  cardInfo: {
+    videoId,
+    uploadThumbNail: { uploadThumbUrl, storeThumbName },
+    title,
+    duration,
+    nickname,
+    view,
+    likes,
+    created_at,
+  },
+}: PropsWithChildren<VideoCardProps>) {
+  return (
+    <VideoCardStyle.Card>
+      <Link href={`/${videoId}`} passHref>
+        <VideoCardStyle.LinkThumbnail>
+          <VideoCardStyle.Thumbnail src={uploadThumbUrl} alt={storeThumbName} />
+        </VideoCardStyle.LinkThumbnail>
+      </Link>
+      <VideoCardStyle.CaptionContainer>
+        <VideoCardStyle.TextCaptionWrapper>
+          <Link href={`/${videoId}`}>
+            <a>
+              <VideoCardStyle.CardTitle>{title}</VideoCardStyle.CardTitle>
+            </a>
+          </Link>
+          <VideoCardStyle.CaptionInfoBox>
+            <span>{nickname}</span>
+            <span>조회수 {view}</span>
+            <span>{created_at}</span>
+          </VideoCardStyle.CaptionInfoBox>
+        </VideoCardStyle.TextCaptionWrapper>
+
+        <VideoCardStyle.LikeButton>
+          <Icon type="heart" width={20} height={20} />
+          <span>{likes}</span>
+        </VideoCardStyle.LikeButton>
+      </VideoCardStyle.CaptionContainer>
+    </VideoCardStyle.Card>
+  );
+}
+
+export default VideoCard;
