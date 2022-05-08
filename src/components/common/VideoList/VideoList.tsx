2e699a3520cf204f4f3906841129a04329936ad1
sonwonjae <sonwj091552@gmail.com>
2022-05-08 22:39:54 +0900

    #26: feat: 비디오 리스트 컴포넌트화

---
+++ src/components/common/VideoList/VideoList.tsx
@@ -0,0 +1,49 @@
+import type { Videos } from '@/types/videos';
+
+import { Fragment, PropsWithChildren, useEffect } from 'react';
+
+import { UseInfiniteQueryResult } from 'react-query';
+import { useInView } from 'react-intersection-observer';
+
+import { VideoCard } from '@components/Common';
+import * as VideoListStyled from './VideoListStyle';
+
+interface VideoListProps {
+  useVideosQueryResult: UseInfiniteQueryResult<
+    {
+      contents: Videos[];
+      nextPage: number;
+      hasMore: boolean;
+    },
+    unknown
+  >;
+}
+
+function VideoList({ useVideosQueryResult: { data, fetchNextPage, hasNextPage } }: PropsWithChildren<VideoListProps>) {
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [inView]);
+
+  return (
+    <VideoListStyled.Videos>
+      {data?.pages.map(({ contents, nextPage }) => (
+        <Fragment key={nextPage}>
+          {contents.map((cardInfo) => (
+            <VideoCard cardInfo={cardInfo} />
+          ))}
+        </Fragment>
+      ))}
+      {hasNextPage && (
+        <li>
+          <VideoListStyled.Observer ref={ref}>불러오는 중...</VideoListStyled.Observer>
+        </li>
+      )}
+    </VideoListStyled.Videos>
+  );
+}
+
+export default VideoList;
