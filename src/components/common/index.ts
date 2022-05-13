ec235a1898b7b9bee4d5be831d544ca690ae22f2
sonwonjae <sonwj091552@gmail.com>
2022-05-14 01:17:18 +0900

    #47 feat: 공통 모달 추가

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -10,3 +10,4 @@ export { default as Title } from './Title/Title';
 export { default as Text } from './Text/Text';
 export { default as Tag } from './Tag/Tag';
 export { default as Textarea } from './Textarea/Textarea';
+export { BottomUp, Alert } from './Modal';
