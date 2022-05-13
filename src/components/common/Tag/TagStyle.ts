b4c12632b016d6cd2c3029ecad95e777585df3e4
Useong Lee <75570915+useonglee@users.noreply.github.com>
2022-05-14 06:59:02 +0900

    Delete src/components/common directory
    (소문자)common 폴더 전부 제거

--- src/components/common/Tag/TagStyle.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const TagStyle = styled.span`
-  padding: 0.6rem 1rem;
-  border: 1px solid ${({ theme }) => theme.color.primary['800']};
-  border-radius: 50px;
-`;
