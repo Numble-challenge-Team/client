5f190f5e172360c0ab2b1ac1e0e6668b47f9ac6b
useonglee <useong0830@naver.com>
2022-05-15 02:11:52 +0900

    #64 feat: 비디오 상세페이지 스타일 수정

--- src/components/Watch/WatchStyle.ts
+++ src/components/Watch/WatchStyle.ts
@@ -2,10 +2,10 @@ import styled from 'styled-components';
 
 export const VideoContainer = styled.section`
   position: sticky;
-  z-index: 100;
+  z-index: 10000;
   top: 0;
-  width: 100%;
   height: 21rem;
+  background-color: ${({ theme }) => theme.color.gray['700']};
 `;
 
 export const VideoDetailInfoContainer = styled.section`
@@ -20,14 +20,14 @@ export const UserInfoWrapper = styled.div`
 
   div:nth-child(1) {
     flex: 2;
+    justify-content: center;
+    align-items: center;
   }
   div:nth-child(2) {
     flex: 12;
   }
   div:nth-child(3) {
     flex: 1;
-    display: flex;
-    flex-direction: column;
     gap: 0.4rem;
 
     & > svg,
