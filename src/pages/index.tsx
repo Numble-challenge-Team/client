c8d40999fedb20c63270bd41a6245ddcde2d07a4
useonglee <useong0830@naver.com>
2022-05-15 02:08:55 +0900

    #64 feat: 쓰지 않는 useState 함수 제거

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
 
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 import Layout from '@components/Layout/Layout';
 import { VideoList } from '@components/Common';
