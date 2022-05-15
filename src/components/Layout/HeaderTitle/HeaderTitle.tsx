189d65f57601a3bdc7b2986ddfe3e51bf1ac816a
useonglee <useong0830@naver.com>
2022-05-15 23:48:21 +0900

    #59 feat: 프로필 페이지 UI 구현 및 로그아웃 기능 구현

--- src/components/Layout/HeaderTitle/HeaderTitle.tsx
+++ src/components/Layout/HeaderTitle/HeaderTitle.tsx
@@ -1,25 +1,41 @@
-import { PropsWithChildren } from 'react';
+import { Dispatch, PropsWithChildren, SetStateAction, useCallback, useState } from 'react';
 import { useRouter } from 'next/router';
 
 import { isValidNormalVideoUploadForm, normalVideoUploadFormData } from '@store/uploadVideo/normalVideo';
 import { isValidEmbedVideoUploadForm, embedVideoUploadFormData } from '@store/uploadVideo/embedVideo';
 import { useRecoilState } from 'recoil';
 
-import { Icon } from '@components/Common';
+import { Icon, Text } from '@components/Common';
+import Drawer from '@components/Common/Drawer/Drawer';
 import { updateVideoIdState } from '@store/videoId';
 import * as HeaderTitleStyled from './HeaderTitleStyle';
 
 interface HeaderTitleProps {
   hasBackButton: boolean;
+  hasSettingButton: boolean;
+  isOpenSettingModal?: boolean;
+  setIsOpenSettingModal?: (newOpen: boolean) => void;
+  isLogout?: boolean;
+  setIsLogout?: Dispatch<SetStateAction<boolean>>;
 }
 
-function HeaderTitle({ hasBackButton, children }: PropsWithChildren<HeaderTitleProps>) {
+function HeaderTitle({
+  hasBackButton,
+  hasSettingButton,
+  isOpenSettingModal = false,
+  setIsOpenSettingModal = () => false,
+  isLogout = false,
+  setIsLogout = (prev) => !prev,
+  children,
+}: PropsWithChildren<HeaderTitleProps>) {
   const router = useRouter();
+
   const [isValidEmbedVideoForm, setIsValidEmbedVideoForm] = useRecoilState(isValidEmbedVideoUploadForm);
   const [embedVideoFormData, setEmbedVideoFormData] = useRecoilState(embedVideoUploadFormData);
   const [isValidNormalVideoForm, setIsValidNormalVideoForm] = useRecoilState(isValidNormalVideoUploadForm);
   const [normalVideoFormData, setNormalVideoFormData] = useRecoilState(normalVideoUploadFormData);
   const [videoId, setVideoId] = useRecoilState(updateVideoIdState);
+
   const resetAllFormData = () => {
     setIsValidEmbedVideoForm(false);
     setEmbedVideoFormData({
@@ -52,6 +68,11 @@ function HeaderTitle({ hasBackButton, children }: PropsWithChildren<HeaderTitleP
     resetAllFormData();
   };
 
+  const handleLogoutUser = useCallback(() => {
+    setIsOpenSettingModal(false);
+    setIsLogout((prev) => !prev);
+  }, [isOpenSettingModal, isLogout]);
+
   return (
     <HeaderTitleStyled.TitleContainer>
       {hasBackButton && (
@@ -60,6 +81,31 @@ function HeaderTitle({ hasBackButton, children }: PropsWithChildren<HeaderTitleP
             <Icon type="circle-arrow-left" />
           </HeaderTitleStyled.BackButton>
         </HeaderTitleStyled.BackButtonWrapper>
+      )}
+      {hasSettingButton && (
+        <HeaderTitleStyled.SettingButtonWrapper>
+          <Drawer
+            icon={{ type: 'setting', width: 24, height: 24 }}
+            height={27}
+            isOpen={isOpenSettingModal}
+            setIsOpen={setIsOpenSettingModal}
+          >
+            <HeaderTitleStyled.SettingMenuWrapper>
+              <HeaderTitleStyled.SettingMenu>
+                <Icon type="edit" />
+                <Text>프로필 수정</Text>
+              </HeaderTitleStyled.SettingMenu>
+              <HeaderTitleStyled.SettingMenu onClick={handleLogoutUser}>
+                <Icon type="logout" />
+                <Text>로그아웃</Text>
+              </HeaderTitleStyled.SettingMenu>
+              <HeaderTitleStyled.SettingMenu>
+                <Icon type="user-delete" />
+                <Text>회원 탈퇴</Text>
+              </HeaderTitleStyled.SettingMenu>
+            </HeaderTitleStyled.SettingMenuWrapper>
+          </Drawer>
+        </HeaderTitleStyled.SettingButtonWrapper>
       )}
       <HeaderTitleStyled.TitleWrapper>{children}</HeaderTitleStyled.TitleWrapper>
     </HeaderTitleStyled.TitleContainer>
---
+++ src/types/profile.ts
@@ -0,0 +1,8 @@
+import { ProfileImgType } from './watch';
+
+export interface UserProfileType {
+  email: string;
+  nickname: string;
+  profileImg: ProfileImgType;
+  usersId: number;
+}
