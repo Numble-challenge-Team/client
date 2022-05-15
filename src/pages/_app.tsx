22da5bf968df57c21c1a0354f87ffaccf782f064
sonwonjae <sonwj091552@gmail.com>
2022-05-16 05:55:29 +0900

    feat: 유효하지 않은 유저는 비회원 권한으로 메인 피드 시청

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,21 +19,6 @@ const queryClient = new QueryClient({
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const pageParameter = router.pathname.split('/')[1];
-  const notTokenPage = ['', 'login', '[signup]', 'watch'];
-
-  useEffect(() => {
-    if (notTokenPage.includes(pageParameter)) {
-      return;
-    }
-
-    if (!localStorage.getItem('accessToken')) {
-      alert('로그인 후 이용해 주세요.');
-      router.push('/login');
-    }
-  }, [pageParameter]);
-
   return (
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
