1479c7051a2b695bb91f1f235277debfa2f178f5
useonglee <useong0830@naver.com>
2022-05-20 00:35:39 +0900

    #103 feat: modal 공통 컴포넌트 스타일 구조 수정

--- src/components/Common/Modal/ModalStyle.ts
+++ src/components/Common/Modal/ModalStyle.ts
@@ -62,42 +62,20 @@ export const AlertModalContainer = styled.div`
   gap: 5rem;
   width: 100%;
   max-width: 33.5rem;
-  /* height: 20rem; */
-  margin: 2rem;
+  padding: 6rem 4rem 2.8rem;
+  font-family: 'NanumSquareR';
   background-color: ${({ theme }) => theme.color.white};
   border-radius: 0.8rem;
 
-  & > * {
-    display: flex;
-    width: 100%;
-    align-items: center;
-    justify-content: center;
-  }
-
-  & > h3 {
-    font-size: 1.6rem;
-    margin-top: 7.2rem;
-  }
-
   & > div {
+    display: flex;
     gap: 1rem;
-    margin-bottom: 2.8rem;
-    & > button {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      width: 100%;
-      max-width: 12rem;
-      line-height: 3.2rem;
-      border-radius: 0.4rem;
-      background-color: ${({ theme }) => theme.color.white};
-      border: 0.1rem solid ${({ theme }) => theme.color.primary[700]};
+  }
 
-      &:first-child {
-        order: 2;
-        background-color: ${({ theme }) => theme.color.primary[700]};
-      }
-    }
+  & > div:nth-child(1) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
   }
 `;
 
