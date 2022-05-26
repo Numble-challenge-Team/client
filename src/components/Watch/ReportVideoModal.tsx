277192048647399e62161580bcce1fbffbec04e6
useonglee <useong0830@naver.com>
2022-05-26 22:56:02 +0900

    #101 feat: 비디오 신고 관련 모달 컴포넌트 추가

---
+++ src/components/Watch/ReportVideoModal.tsx
@@ -0,0 +1,57 @@
+import { Alert, Button, Text } from '@components/Common';
+import React from 'react';
+
+interface ReportVideoModalPropsType {
+  isShowReportVideoModal: boolean;
+  isShowSuccessModal: boolean;
+  isShowFailedModal: boolean;
+  handleReportVideoButton: (type: 'open' | 'close' | 'submit') => void;
+}
+
+function ReportVideoModal({
+  isShowReportVideoModal,
+  isShowSuccessModal,
+  isShowFailedModal,
+  handleReportVideoButton,
+}: ReportVideoModalPropsType) {
+  return (
+    <>
+      {isShowReportVideoModal && (
+        <Alert onBlurModal={() => handleReportVideoButton('close')}>
+          <div>
+            <Text>영상을 신고하시겠어요?</Text>
+          </div>
+          <div>
+            <Button type="button" size="S" backColor="border" clickEvent={() => handleReportVideoButton('close')}>
+              취소
+            </Button>
+            <Button type="button" size="S" backColor="primary" clickEvent={() => handleReportVideoButton('submit')}>
+              확인
+            </Button>
+          </div>
+        </Alert>
+      )}
+      {isShowSuccessModal && (
+        <Alert onBlurModal={() => handleReportVideoButton('close')}>
+          <div>
+            <Text>신고가 완료되었습니다.</Text>
+            <Text>콘텐츠 유해성 확인 후 조치하겠습니다.</Text>
+          </div>
+          <Button type="button" size="M" backColor="primary" clickEvent={() => handleReportVideoButton('close')}>
+            확인
+          </Button>
+        </Alert>
+      )}
+      {isShowFailedModal && (
+        <Alert onBlurModal={() => handleReportVideoButton('close')}>
+          <Text>신고는 한 번만 가능합니다.</Text>
+          <Button type="button" size="M" backColor="primary" clickEvent={() => handleReportVideoButton('close')}>
+            확인
+          </Button>
+        </Alert>
+      )}
+    </>
+  );
+}
+
+export default ReportVideoModal;
